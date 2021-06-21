a764b4bbb9662fda8d84bbeab598c17d2a7d2712
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-06-21 09:14:21 +0300

    check for secure connection

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const csp = require('express-csp');
 
 const app = express();
 
+app.enable('trust proxy');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -12,14 +12,12 @@ const signToken = id => {
     });
 }
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
     const token = signToken(user._id);
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true
-    }
-    if (process.env.NODE_ENV === 'production') {
-        cookieOptions.secure = true;
+        httpOnly: true,
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
     }
 
     res.cookie('jwt', token, cookieOptions);
@@ -48,7 +46,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     console.log(url)
     await new Email(newUser, url).sendWelcome();
     
-    createSendToken(newUser, 201, res);
+    createSendToken(newUser, 201, req, res);
 })
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -65,7 +63,7 @@ exports.login = catchAsync(async (req, res, next) => {
         return next(new AppError('Wrong email or password!', 401));
     }
 
-    createSendToken(user, 200, res);
+    createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
@@ -191,7 +189,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
     await user.save();
     //3. Log user in
-    createSendToken(user, 200, res);
+    createSendToken(user, 200, req, res);
 
 })
 
@@ -213,5 +211,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
         await user.save();
     // 4. Log in the user, send JWT
-    createSendToken(user, 200, res);
+    createSendToken(user, 200, req, res);
 })
